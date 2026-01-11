d9a12a1f44ddd2c1afd3b4d170094cb7a5f1b4f3
Ankit-Kumar-108 <smileymandss786@gmail.com>
2026-01-11 23:27:06 +0530

    minor deploy error fix

--- src/app/admin/notices/page.tsx
+++ src/app/admin/notices/page.tsx
@@ -2,7 +2,9 @@
 
 export default function Notice(){
     return(
-        <div>
+        <div className="w-dvw h-dvh bg-amber-300 flex justify-center items-center">
+            <h1>Notice</h1>
+            <p>Here will be notice</p>
         </div>
     )
 }
--- src/app/admin/students/page.tsx
+++ src/app/admin/students/page.tsx
@@ -1,8 +1,10 @@
 
 
-export default function Notice(){
+export default function Student(){
     return(
-        <div>
+        <div className="w-dvw h-dvh bg-amber-300 flex justify-center items-center">
+            <h1>students</h1>
+            <p>Here will be students</p>
         </div>
     )
 }
