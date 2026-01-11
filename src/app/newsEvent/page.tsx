867e3dcaa07103e12da25ab26fb22cbad2baceaf
Ankit-Kumar-108 <smileymandss786@gmail.com>
2026-01-11 23:30:52 +0530

    minor fix

---
+++ src/app/newsEvent/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NewsEvent(){
+    return(
+        <div className="w-dvw h-dvh bg-amber-300 flex justify-center items-center">
+            <h1>News and Event</h1>
+            <p>Here will be News and Event</p>
+        </div>
+    )
+}
---
+++ src/app/notice/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function noticess(){
+    return(
+        <div className="w-dvw h-dvh bg-amber-300 flex justify-center items-center">
+            <h1>notice</h1>
+            <p>Here will be noticess</p>
+        </div>
+    )
+}
